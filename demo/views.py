c88947a09f230f0e41534ab3858c9d765896b6a4
karthideva444 <ksrthideva444@gmail.com>
2024-03-31 12:39:38 +0530

    view.py message printed

--- demo/views.py
+++ demo/views.py
@@ -94,4 +94,5 @@ def student_create(request):
             return HttpResponse(json_data,content_type='application/json')
         return HttpResponse(JSONRenderer().render(serializer.errors),content_type='application/json')
 
+print("added message from karthick branch")
 
